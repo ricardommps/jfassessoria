9a7af8b14fefdf85fd87c5a93c5e5029015eed12
ricardo.machado <ricardo.machado@paygo-consult.com.br>
2023-08-24 10:17:25 -0300

    add search customer and program

--- src/sections/race-consulting/program/programs-list.js
+++ src/sections/race-consulting/program/programs-list.js
@@ -1,8 +1,14 @@
 import Grid from '@mui/material/Unstable_Grid2';
 import PropTypes from 'prop-types';
+import { useCallback, useState } from 'react';
 import useProgram from 'src/hooks/use-program';
 
 import ProgramItem from './program-item';
+import ProgramSearch from './program-search';
+
+const defaultFilters = {
+  name: '',
+};
 
 export default function ProgramasList({
   onSelectedProgram,
@@ -11,21 +17,38 @@ export default function ProgramasList({
   sendProgramStatus,
 }) {
   const { programs, onCloneProgram, onDeleteProgram } = useProgram();
+  const [filters, setFilters] = useState(defaultFilters);
+
+  const dataFiltered = applyFilter({
+    inputData: programs,
+    filters,
+  });
+
+  const handleFilters = useCallback((name, value) => {
+    setFilters((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }, []);
+
   return (
-    <Grid container spacing={2} pt={3}>
+    <Grid container spacing={2} pt={1}>
       <Grid xs={12} sm={12} md={12}>
-        {programs?.map((program) => (
-          <ProgramItem
-            key={program.id}
-            program={program}
-            onCloneProgram={onCloneProgram}
-            onSelectedProgram={onSelectedProgram}
-            onSendProgram={handleOpenSend}
-            onDeleteProgram={onDeleteProgram}
-            cloneProgramStatus={cloneProgramStatus}
-            sendProgramStatus={sendProgramStatus}
-          />
-        ))}
+        <>
+          <ProgramSearch filters={filters} onFilters={handleFilters} />
+          {dataFiltered?.map((program) => (
+            <ProgramItem
+              key={program.id}
+              program={program}
+              onCloneProgram={onCloneProgram}
+              onSelectedProgram={onSelectedProgram}
+              onSendProgram={handleOpenSend}
+              onDeleteProgram={onDeleteProgram}
+              cloneProgramStatus={cloneProgramStatus}
+              sendProgramStatus={sendProgramStatus}
+            />
+          ))}
+        </>
       </Grid>
     </Grid>
   );
@@ -34,3 +57,18 @@ export default function ProgramasList({
 ProgramasList.propTypes = {
   onSelectedProgram: PropTypes.func,
 };
+
+function applyFilter({ inputData, filters }) {
+  const { name } = filters;
+
+  const stabilizedThis = inputData.map((el, index) => [el, index]);
+
+  inputData = stabilizedThis.map((el) => el[0]);
+
+  if (name) {
+    inputData = inputData.filter(
+      (customer) => customer.name.toLowerCase().indexOf(name.toLowerCase()) !== -1,
+    );
+  }
+  return inputData;
+}
