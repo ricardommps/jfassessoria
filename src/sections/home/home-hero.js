f3abdb3f571359c098e6600773939e92471157d3
Ricardo Matta <ricardommps@gmail.com>
2024-10-07 22:47:38 -0300

    build fix

--- src/sections/home/home-hero.js
+++ src/sections/home/home-hero.js
@@ -2,9 +2,9 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
+import { alpha, styled, useTheme } from '@mui/material/styles';
 // @mui
 import Grid from '@mui/material/Unstable_Grid2';
-import { alpha, styled, useTheme } from '@mui/material/styles';
 import { m, useScroll } from 'framer-motion';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { MotionContainer, varFade } from 'src/components/animate';
