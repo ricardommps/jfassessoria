c45adc907e719ed6b14448017502ba250f47707d
Ricardo Matta <ricardommps@gmail.com>
2024-10-27 21:19:48 -0300

    fix build

--- src/sections/customer/view/index.js
+++ src/sections/customer/view/index.js
@@ -1,3 +1,4 @@
 export { default as CustomerView } from './customer-list-view';
 export { default as CustomerProfileView } from './customer-profile-view';
+// eslint-disable-next-line prettier/prettier
 
