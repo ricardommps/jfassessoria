a99776cb67456ba4612b2cf2d09b99c3ab8b6af6
Ricardo Matta <ricardommps@gmail.com>
2026-03-23 21:00:25 -0300

    bug fix media

--- src/hooks/use-medias.js
+++ src/hooks/use-medias.js
@@ -1,9 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
-import { getMedias } from 'src/services/media.service';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { create, getMedias } from 'src/services/media.service';
 
 export function useGetMedias() {
   return useQuery({
     queryKey: ['medias'],
     queryFn: getMedias,
+  });
+}
+
+export function useCreateMedia() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: create,
+    onSuccess: () => {
+      // Invalida a query de medias para refetch automático
+      queryClient.invalidateQueries({ queryKey: ['medias'] });
+    },
   });
 }
