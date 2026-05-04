3766286c74a5799179e0af580265314da8da0faf
Ricardo Matta <ricardommps@gmail.com>
2026-05-04 10:09:44 -0300

    Ajustes de cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   trailingSlash: true,
+
   modularizeImports: {
     '@mui/material': {
       transform: '@mui/material/{{member}}',
@@ -8,28 +9,17 @@ module.exports = {
       transform: '@mui/lab/{{member}}',
     },
   },
+
   images: {
-    domains: ['supertreinosapp.com']
+    domains: ['supertreinosapp.com'],
   },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
     return config;
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
 };
